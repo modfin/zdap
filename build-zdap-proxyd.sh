c3205250a4d591534ec4701fcfa3a8fdc042bebd
raz <rasmus.holm@modularfinance.se>
2022-10-11 09:20:13 +0200

    adding multi arch

--- build-zdap-proxyd.sh
+++ build-zdap-proxyd.sh
@@ -3,12 +3,10 @@
 VERSION=$(date +%Y-%m-%dT%H.%M.%S)-$(git log -1 --pretty=format:"%h")
 IMAGE_NAME=crholm/zdap-proxy
 
-docker build -f Dockerfile.zdap-proxy \
+docker buildx build -f Dockerfile.zdap-proxy \
+    --push \
+    --platform=linux/amd64,linux/arm64 \
     -t ${IMAGE_NAME}:latest \
     -t ${IMAGE_NAME}:${VERSION} \
     . || exit 1
-
-
-docker push ${IMAGE_NAME}:${VERSION}
-docker push ${IMAGE_NAME}:latest
 
