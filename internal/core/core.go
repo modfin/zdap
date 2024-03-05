186a67b8f47e26434d0034906b585d16881b8ee8
Marcus Widegren <122600249+modfinmarwi@users.noreply.github.com>
2024-03-05 12:07:35 +0100

    Fix nil pointer when there is no clone pool configured (#13)
    Fix nil pointer when there is no clone pool configured

--- internal/core/core.go
+++ internal/core/core.go
@@ -405,9 +405,15 @@ func (c *Core) ServerStatus(dss *zfs.Dataset) (zdap.ServerStatus, error) {
 	s.ResourceDetails = make(map[string]zdap.ServerResourceDetails)
 	for _, r := range c.resources {
 		s.Resources = append(s.Resources, r.Name)
+		pool := c.clonePools[r.Name]
+		available := 0
+		if pool != nil {
+			available = pool.ClonesAvailable
+		}
+
 		s.ResourceDetails[r.Name] = zdap.ServerResourceDetails{
 			Name:                  r.Name,
-			PooledClonesAvailable: c.clonePools[r.Name].ClonesAvailable,
+			PooledClonesAvailable: available,
 		}
 	}
 
