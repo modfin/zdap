6432baf9d62637236ac3162c3cc3460fc6061570
Lars-Erik Svensson <lars-erik.svensson@modularfinance.se>
2024-06-07 17:28:10 +0200

    Update zdap-proxy dependencies to fix various CVE's

--- build-zdap-proxyd.sh
+++ build-zdap-proxyd.sh
@@ -3,10 +3,13 @@
 VERSION=$(date +%Y-%m-%dT%H.%M.%S)-$(git log -1 --pretty=format:"%h")
 IMAGE_NAME=crholm/zdap-proxy
 
-docker buildx build -f Dockerfile.zdap-proxy \
+BUILDER=$(docker buildx create) || exit 1
+
+docker buildx build --builder=${BUILDER} -f Dockerfile.zdap-proxy \
     --push \
     --platform=linux/amd64,linux/arm64 \
     -t ${IMAGE_NAME}:latest \
     -t ${IMAGE_NAME}:${VERSION} \
     . || exit 1
 
+docker buildx rm "${BUILDER}"
